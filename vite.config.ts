c1388f16380abc3ed2a7af10a7e12ddaeb69336b
DESKTOP-F1HUGFM\mikke <mikkelhn1@hotmail.com>
2023-03-12 11:34:10 +0100

    Adding manualchunks config to vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), basicSsl()],
+  plugins: [vue(), basicSsl(), splitVendorChunkPlugin()],
   server: {
     port: 3400,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'mapbox-gl': ['mapbox-gl'],
+          'vue-mapbox-gl': ['@studiometa/vue-mapbox-gl'],
+          '@mapbox': ['@mapbox'],
+        },
+      },
+    },
   },
 });
